88420e137e21a674a65d1486fcb40e9ba43a5cc1
Sogo Ogundowole <dowolebolu@gmail.com>
2022-11-29 22:43:17 +0100

    chore: added parmeter store to retrieve db password

--- settings.py
+++ settings.py
@@ -1,5 +1,14 @@
 import os
 import dj_database_url  # type: ignore
+import boto3
+
+# Get db paasword from AWS SSM
+session = boto3.Session(profile_name='civic-eagle')
+ssm = session.client('ssm')
+
+parameter = ssm.get_parameter(Name=os.environ.get("STAGE_PARAM_NAME"), WithDecryption=True)
+POSTGRES_PASSWORD = parameter['Parameter']['Value']
+
 
 # django settings for tests
 SECRET_KEY = "test"
@@ -7,13 +16,15 @@ INSTALLED_APPS = (
     "django.contrib.contenttypes",
     "openstates.data",
 )
+
+
 MIDDLEWARE_CLASSES = ()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_NAME", "test"),
         "USER": os.environ.get("POSTGRES_USER", "test"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "test"),
+        "PASSWORD": POSTGRES_PASSWORD,
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
